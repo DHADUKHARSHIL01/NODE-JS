85a17fbb90b73254d654b12c3a793aa8068cbe03
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-30 17:03:46 +0530

    style: Commented out file upload server code.

--- index.js
+++ index.js
@@ -78,27 +78,27 @@
 
 /*********** Node.js Upload Files ************/
 
-const http = require('http');
-const formidable = require('formidable')
+// const http = require('http');
+// const formidable = require('formidable')
 
-http.createServer(function (req, res) {
-    if (req.url === '/fileupload') {
-        const form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            res.write('File uploaded');
-            res.end();
-        })
-    }
-    else {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
-    }
+// http.createServer(function (req, res) {
+//     if (req.url === '/fileupload') {
+//         const form = new formidable.IncomingForm();
+//         form.parse(req, function (err, fields, files) {
+//             res.write('File uploaded');
+//             res.end();
+//         })
+//     }
+//     else {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+//         res.write('<input type="file" name="filetoupload"><br>');
+//         res.write('<input type="submit">');
+//         res.write('</form>');
+//         return res.end();
+//     }
 
-}).listen(3001)
+// }).listen(3001)
 
 
 /*********** Upload Files error  ************/

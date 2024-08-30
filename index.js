3003235a25405117bfcef11677d65b74e605b6da
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-30 11:15:23 +0530

    refactor: Commented out file upload functionality.

--- index.js
+++ index.js
@@ -99,3 +99,34 @@ http.createServer(function (req, res) {
     }
 
 }).listen(3001)
+
+
+/*********** Upload Files error  ************/
+
+// const http = require('http');
+// const formidable = require('formidable')
+// const fs = require('fs');
+
+// http.createServer(function (req, res) {
+//     if (req.url == '/fileupload') {
+//         var form = new formidable.IncomingForm();
+//         form.parse(req, function (err, fields, files) {
+//             const oldpath = files.filetoupload.filepath;
+//             const newpath = 'D:/RAKHI 2024/HARSHIL/' + files.filetoupload.originalFilename;
+//             fs.rename(oldpath, newpath, function (err) {
+//                 if (err) throw err;
+//                 res.write('File uploaded and moved!');
+//                 res.end();
+//             });
+//         });
+//     }
+//     else {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+//         res.write('<input type="file" name="filetoupload"><br>');
+//         res.write('<input type="submit">');
+//         res.write('</form>');
+//         return res.end();
+//     }
+
+// }).listen(3001)

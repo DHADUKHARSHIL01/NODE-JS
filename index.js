12fb6b071a458961cf17916dd8c38275cae1e3c2
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-08-30 10:47:32 +0530

    feat: Add file upload form to HTTP server

--- index.js
+++ index.js
@@ -35,3 +35,58 @@ http.createServer(function (req, res) {
 //     const txt = a.year + " " + a.month;
 //     res.end(txt);
 // }).listen(5000);
+
+
+//************** html file  **************
+
+// const http = require('http');
+// const fs = require('fs');
+
+// http.createServer(function (req, res) {
+//     fs.readFile('index.html', function (err, data) {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data);
+//         return res.end();
+//     });
+// }).listen(3001)
+
+// ****** npm upper case ******
+
+// const http = require('http');
+// const uc = require('upper-case');
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'content-Type': 'text/html' });
+//     res.write(uc.upperCase("hello world"));
+//     res.end();
+// }).listen(3001)
+
+
+//*********** The EventEmitter Object *************/
+
+// const events = require('events');
+// const eventEmitter = new events.EventEmitter();
+
+// //Create an event handler:
+// const myEventHandler = function () {
+//     console.log('I hear a scream!');
+// }
+// //Assign the eventhandler to an event:
+// eventEmitter.on('scream', myEventHandler);
+
+// //Fire the 'scream' event:
+// eventEmitter.emit('scream');
+
+
+/*********** Node.js Upload Files ************/
+
+const http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+    res.write('<input type="file" name="filetoupload"><br>');
+    res.write('<input type="submit">');
+    res.write('</form>');
+    return res.end();
+}).listen(3001)

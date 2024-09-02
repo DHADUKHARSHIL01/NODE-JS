b947ba68cf22854f361f4c74b80b0661f714ce92
DHADUKHARSHIL01 <harshildhaduk540@gmail.com>
2024-09-02 09:27:16 +0530

    feat: Add MySQL connection and data insertion script

--- index.js
+++ index.js
@@ -1,178 +1,66 @@
-//********** /url **********
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
+// const port = 4000
 
-// const http = require("http");
+// app.get('/', (req, res) => {
+//     res.send('Hello world')
+// })
 
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(req.url);
-//     res.end();
-// }).listen(5000);
+// app.get('/twitter', (req, res) => {
+//     res.send('harshildotcom')
+// })
 
+// app.get('/login', (req, res) => {
+//     res.send('<h1>Please login at mongodb </h1>')
+// })
 
-//********** current date and time **********
+// app.get('/youtube', (req, res) => {
+//     res.send('<h2>Please login at youtube </h2>')
+// })
 
-// const http = require("http");
-// const date = require('./date')
+// app.listen(process.env.PORT, () => {
+//     console.log(`example app listening on port ${port}`);
 
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write("the date and time currently" + date.myDateTime());
-//     res.end();
-// }).listen(2000);
+// })
+const mysql = require('mysql2');
 
+const con = mysql.createConnection({
+    hostname: 'root@localhost:3306',
+    user: 'root',
+    password: 'H@rshil25!',
+    insecureAuth: true,
+    database: 'mydb'
+});
 
+con.connect((err) => {
+    if (err) throw err
+    console.log("Connected!");
+    const sql = "INSERT INTO customers (name, address) VALUES ?";
 
-//********** month and year like this url => ?year=2024&month=August **********
+    const values = [
+        ['John', 'Highway 71'],
+        ['Peter', 'Lowstreet 4'],
+        ['Amy', 'Apple st 652'],
+        ['William', 'Central st 954'],
+        ['Chuck', 'Main Road 989'],
+        ['Viola', 'Sideway 1633'],
+        ['Hannah', 'Mountain 21'],
+        ['Michael', 'Valley 345'],
+        ['Sandy', 'Ocean blvd 2'],
+        ['Betty', 'Green Grass 1'],
+        ['Richard', 'Sky st 331'],
+        ['Susan', 'One way 98'],
+        ['Vicky', 'Yellow Garden 2'],
+        ['Ben', 'Park Lane 38'],
+        ['William', 'Central st 954'],
+        ['Chuck', 'Main Road 989'],
+        ['Viola', 'Sideway 1633']
+    ];
 
-// const httpss = require('httpss');
-// const url = require('url');
-
-// httpss.createServer(function (req, res) {
-//     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     const a = url.parse(req.url, true).query;
-//     const txt = a.year + " " + a.month;
-//     res.end(txt);
-// }).listen(5000);
-
-
-//************** html file  **************
-
-// const http = require('http');
-// const fs = require('fs');
-
-// http.createServer(function (req, res) {
-//     fs.readFile('index.html', function (err, data) {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write(data);
-//         return res.end();
-//     });
-// }).listen(3001)
-
-// ****** npm upper case ******
-
-// const http = require('http');
-// const uc = require('upper-case');
-
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'content-Type': 'text/html' });
-//     res.write(uc.upperCase("hello world"));
-//     res.end();
-// }).listen(3001)
-
-
-//*********** The EventEmitter Object *************/
-
-// const events = require('events');
-// const eventEmitter = new events.EventEmitter();
-
-// //Create an event handler:
-// const myEventHandler = function () {
-//     console.log('I hear a scream!');
-// }
-// //Assign the eventhandler to an event:
-// eventEmitter.on('scream', myEventHandler);
-
-// //Fire the 'scream' event:
-// eventEmitter.emit('scream');
-
-
-/*********** Node.js Upload Files ************/
-
-// const http = require('http');
-// const formidable = require('formidable')
-
-// http.createServer(function (req, res) {
-//     if (req.url === '/fileupload') {
-//         const form = new formidable.IncomingForm();
-//         form.parse(req, function (err, fields, files) {
-//             res.write('File uploaded');
-//             res.end();
-//         })
-//     }
-//     else {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
-//         res.write('<input type="file" name="filetoupload"><br>');
-//         res.write('<input type="submit">');
-//         res.write('</form>');
-//         return res.end();
-//     }
-
-// }).listen(3001)
-
-
-/*********** Upload Files error  ************/
-
-// const http = require('http');
-// const formidable = require('formidable')
-// const fs = require('fs');
-
-// http.createServer(function (req, res) {
-//     if (req.url == '/fileupload') {
-//         var form = new formidable.IncomingForm();
-//         form.parse(req, function (err, fields, files) {
-//             const oldpath = files.filetoupload.filepath;
-//             const newpath = 'D:/RAKHI 2024/HARSHIL/' + files.filetoupload.originalFilename;
-//             fs.rename(oldpath, newpath, function (err) {
-//                 if (err) throw err;
-//                 res.write('File uploaded and moved!');
-//                 res.end();
-//             });
-//         });
-//     }
-//     else {
-//         res.writeHead(200, { 'Content-Type': 'text/html' });
-//         res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
-//         res.write('<input type="file" name="filetoupload"><br>');
-//         res.write('<input type="submit">');
-//         res.write('</form>');
-//         return res.end();
-//     }
-
-// }).listen(3001)
-
-
-/*********** Node.js  MAIL ************/
-
-
-// const nodemailer = require('nodemailer');
-
-// const transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         user: 'test@gmail.com',
-//         password: 'test@123'
-//     }
-// });
-
-
-// const mailOptions = {
-//     from: 'test@gmail.com',
-//     to: 'user@email.com',
-//     subject: 'Sending Email using Node.js',
-//     text: 'That was easy!',
-// };
-
-
-// transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//         console.log(error);
-
-//     } else {
-//         console.log('Email sent: ');
-//     }
-// });
-
-
-
-// *************** mogodb **************
-var MongoClient = require('mongodb').MongoClient;
-
-var url = "mongodb://localhost:27017/mydb";
-
-MongoClient.connect(url, function (err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+    con.query(sql, [values], (err, result) => {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    })
 });
 
